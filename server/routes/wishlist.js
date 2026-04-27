976b4b6756bed1d782fbc102f6596dfa294b10d0
Danaboyina Abhiram <danaboyinaabhiram@gmail.com>
2026-04-27 05:58:21 +0530

    Delete server/routes/wishlist.js

--- server/routes/wishlist.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getWishlist, toggleWishlist } = require('../controllers/wishlistController');
-const { protect, approvedAccount } = require('../middleware/authMiddleware');
-
-router.get('/', protect, approvedAccount, getWishlist);
-router.post('/toggle', protect, approvedAccount, toggleWishlist);
-
-module.exports = router;
